36ef7242e30405b40695bbbeb0be9f26b95abd6a
Ian Johnson <tacoda@pm.me>
2018-06-03 13:09:23 -0400

    First successful dynamodb create table test

--- src/jaws/dynamodb.rs
+++ src/jaws/dynamodb.rs
@@ -1,5 +1,5 @@
 use rusoto_core::Region;
-use rusoto_dynamodb::{DynamoDb, DynamoDbClient, CreateTableInput, ListTablesInput};
+use rusoto_dynamodb::*;
 
 pub fn list() {
         let client = DynamoDbClient::simple(Region::UsEast1);
@@ -26,9 +26,25 @@ pub fn list() {
 
 pub fn create(name: String) {
     let client = DynamoDbClient::simple(Region::UsEast1);
-    let mut create_table_input: CreateTableInput = Default::default();
-    create_table_input.table_name = name;
-    
+    let create_table_input: CreateTableInput = CreateTableInput {
+        attribute_definitions: vec![AttributeDefinition {
+            attribute_name: String::from("key"),
+            attribute_type: String::from("S"),
+        }],
+        global_secondary_indexes: None,
+        key_schema: vec![KeySchemaElement {
+            attribute_name: String::from("key"),
+            key_type: String::from("HASH"),
+        }],
+        local_secondary_indexes: None,
+        provisioned_throughput: ProvisionedThroughput {
+            read_capacity_units: 4,
+            write_capacity_units: 4,
+        },
+        stream_specification: None,
+        table_name: name,
+    };
+
     match client.create_table(&create_table_input).sync() {
         Ok(output) => {
             match output.table_description {
