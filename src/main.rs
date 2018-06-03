fb42feb9c1800e87c6d803033356f0a410819c4c
Ian Johnson <tacoda@pm.me>
2018-06-03 11:22:41 -0400

    Add dynamo table list functionality back in

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #[macro_use]
 extern crate structopt;
+extern crate rusoto_core;
+extern crate rusoto_dynamodb;
+mod jaws;
 
 // use std::path::PathBuf;
 use structopt::StructOpt;
+use jaws::dynamo;
 
 #[derive(StructOpt)]
 #[structopt(name = "jaws", about = "AWS management tool and task runner")]
@@ -29,65 +33,10 @@ enum Jaws {
     // }
 }
 
-// TODO: Transition strings to enums
-// #[derive(Debug)]
-// enum DynamoDbOption {
-//     Create,
-//     List,
-// }
-
 fn main() {
     let jaws = Jaws::from_args();
-    // println!("{:?}", jaws);
 
     match jaws {
-        Jaws::DynamoDb { list } => println!("list is {}!", list)
+        Jaws::DynamoDb { list } => if list { dynamo::list() }
     }
-
-    // if
 }
-
-// extern crate clap;
-//
-// extern crate rusoto_core;
-// extern crate rusoto_dynamodb;
-// mod jaws;
-//
-// use clap::{App, Arg, SubCommand};
-// use jaws::dynamo;
-
-// fn main() {
-
-//     let matches = App::new("jaws")
-//                           .version("0.1")
-//                           .author("Ian Johnson <tacoda@pm.me>")
-//                           .about("AWS CLI and task runner")
-//                           // .arg(Arg::with_name("v")
-//                           //      .short("v")
-//                           //      .multiple(true)
-//                           //      .help("Sets the level of verbosity"))
-//                           .subcommand(SubCommand::with_name("dynamo")
-//                                       .about("DynamoDb")
-//                                       .subcommand(SubCommand::with_name("list")
-//                                                   .about("List"))
-//                                       .subcommand(SubCommand::with_name("create")
-//                                                   .about("Create")
-//                                                   .arg(Arg::with_name("NAME")
-//                                                         .help("Sets the name of the new table")
-//                                                         .required(true))))
-//                           .get_matches();
-//
-// // TODO: Refactor this to matches with guards
-//
-//     println!("Using input file: {}", matches.value_of("NAME").unwrap());
-//
-//     if let Some(supercommand) = matches.subcommand_matches("dynamo") {
-//         if let Some(_subcommand) = supercommand.subcommand_matches("list") {
-//             dynamo::list();
-//         }
-//         if let Some(_subcommand) = supercommand.subcommand_matches("create") {
-//             dynamo::create();
-//         }
-//     }
-
-// }
