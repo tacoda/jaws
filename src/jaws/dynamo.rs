6a580c4d294d976c1ca28aec56957c123b2dc3dc
Ian Johnson <tacoda@pm.me>
2018-06-03 10:37:20 -0400

    Transition to structopt

--- src/jaws/dynamo.rs
+++ src/jaws/dynamo.rs
@@ -1,7 +1,7 @@
 use rusoto_core::Region;
-use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ListTablesInput};
+use rusoto_dynamodb::{DynamoDb, DynamoDbClient, CreateTableInput, ListTablesInput};
 
-pub fn ls() {
+pub fn list() {
         let client = DynamoDbClient::simple(Region::UsEast1);
         let list_tables_input: ListTablesInput = Default::default();
 
@@ -22,4 +22,24 @@ pub fn ls() {
                 println!("Error: {:?}", error);
             },
         }
+}
+
+pub fn create() {
+    // let client = DynamoDbClient::simple(Region::UsEast1);
+    // let create_table_input: CreateTableInput = Default::default();
+    //
+    // match client.create_table(&create_table_input).sync() {
+    //     Ok(output) => {
+    //         match output.table_description {
+    //             Some(table_description) => {
+    //                 println!("Table description:");
+    //                 println!("{:?}", table_description);
+    //             },
+    //             None => println!("Table not created!"),
+    //         }
+    //     },
+    //     Err(error) => {
+    //         println!("Error: {:?}", error);
+    //     },
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,72 @@
-extern crate clap;
+#[macro_use]
+extern crate structopt;
 
-extern crate rusoto_core;
-extern crate rusoto_dynamodb;
-mod jaws;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
-use clap::{App, SubCommand};
-use jaws::dynamo;
+#[derive(Debug, StructOpt)]
+#[structopt(name = "jaws", about = "AWS management tool and task runner")]
+struct Jaws {
+    /// Activate debug mode
+    #[structopt(short = "d", long = "debug")]
+    debug: bool,
+    /// Set speed
+    #[structopt(short = "s", long = "speed", default_value = "42")]
+    speed: f64,
+    /// Input file
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+    /// Output file, stdout if not present
+    #[structopt(parse(from_os_str))]
+    output: Option<PathBuf>,
+}
 
 fn main() {
-    let matches = App::new("jaws")
-                          .version("0.1")
-                          .author("Ian Johnson <tacoda@pm.me>")
-                          .about("AWS CLI and task runner")
-                          // .arg(Arg::with_name("v")
-                          //      .short("v")
-                          //      .multiple(true)
-                          //      .help("Sets the level of verbosity"))
-                          .subcommand(SubCommand::with_name("dynamo")
-                                      .about("DynamoDb")
-                                      .subcommand(SubCommand::with_name("ls")
-                                                  .about("List")))
-                          .get_matches();
-
-    if let Some(supercommand) = matches.subcommand_matches("dynamo") {
-        if let Some(subcommand) = supercommand.subcommand_matches("ls") {
-            dynamo::ls();
-        }
-    }
-
+    let jaws = Jaws::from_args();
+    println!("{:?}", jaws);
 }
+
+// extern crate clap;
+//
+// extern crate rusoto_core;
+// extern crate rusoto_dynamodb;
+// mod jaws;
+//
+// use clap::{App, Arg, SubCommand};
+// use jaws::dynamo;
+
+// fn main() {
+
+//     let matches = App::new("jaws")
+//                           .version("0.1")
+//                           .author("Ian Johnson <tacoda@pm.me>")
+//                           .about("AWS CLI and task runner")
+//                           // .arg(Arg::with_name("v")
+//                           //      .short("v")
+//                           //      .multiple(true)
+//                           //      .help("Sets the level of verbosity"))
+//                           .subcommand(SubCommand::with_name("dynamo")
+//                                       .about("DynamoDb")
+//                                       .subcommand(SubCommand::with_name("list")
+//                                                   .about("List"))
+//                                       .subcommand(SubCommand::with_name("create")
+//                                                   .about("Create")
+//                                                   .arg(Arg::with_name("NAME")
+//                                                         .help("Sets the name of the new table")
+//                                                         .required(true))))
+//                           .get_matches();
+//
+// // TODO: Refactor this to matches with guards
+//
+//     println!("Using input file: {}", matches.value_of("NAME").unwrap());
+//
+//     if let Some(supercommand) = matches.subcommand_matches("dynamo") {
+//         if let Some(_subcommand) = supercommand.subcommand_matches("list") {
+//             dynamo::list();
+//         }
+//         if let Some(_subcommand) = supercommand.subcommand_matches("create") {
+//             dynamo::create();
+//         }
+//     }
+
+// }
