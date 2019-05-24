0d516864f802af2d2c2f4f587e4c9fc3299ce466
Ian Johnson <tacoda@pm.me>
2019-05-23 22:00:32 -0400

    Add notes for CLI best practices

--- src/main.rs
+++ src/main.rs
@@ -102,3 +102,65 @@ fn main() {
         }
     }
 }
+
+// Notes
+
+// use failure::ResultExt;
+// use exitfailure::ExitFailure;
+//
+// fn main() -> Result<(), ExitFailure> {
+//
+//     let path = "test.txt";
+//     let content = std::fs::read_to_string(path)
+//         .with_context(|_| format!("could not read file `{}`", path))?;
+//     println!("file content: {}", content);
+//     Ok(())
+// }
+
+// Printing errors:
+// println!("This is information");
+// eprintln!("This is an error! :(");
+
+// Keep IO from becoming a bottleneck
+// use std::io::{self, Write};
+//
+// let stdout = io::stdout(); // get the global stdout entity
+// let mut handle = io::BufWriter::new(stdout); // optional: wrap that handle in a buffer
+// writeln!(handle, "foo: {}", 42); // add `?` if you care about errors here
+
+// use std::io::{self, Write};
+//
+// let stdout = io::stdout(); // get the global stdout entity
+// let mut handle = stdout.lock(); // acquire a lock on it
+// writeln!(handle, "foo: {}", 42); // add `?` if you care about errors here
+
+// Showing a progress bar
+// indicatif crate
+//
+// fn main() {
+//     let pb = indicatif::ProgressBar::new(100);
+//     for i in 0..100 {
+//         do_hard_work();
+//         pb.println(format!("[+] finished #{}", i));
+//         pb.inc(1);
+//     }
+//     pb.finish_with_message("done");
+// }
+
+// Logging
+// log crate
+// env_logger crate
+// use log::{info, warn};
+//
+// fn main() {
+//     env_logger::init();
+//     info!("starting up");
+//     warn!("oops, nothing implemented!");
+// }
+
+// Run in terminal using this
+// $ env RUST_LOG=output_log=info cargo run --bin output-log
+//     Finished dev [unoptimized + debuginfo] target(s) in 0.17s
+//      Running `target/debug/output-log`
+// [2018-11-30T20:25:52Z INFO  output_log] starting up
+// [2018-11-30T20:25:52Z WARN  output_log] oops, nothing implemented!
